bb068a607c17cd593c4917149538c99cb0e66030
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-02 20:54:33 -0300

    v0.0.0.2 - Creating login validation
    This time we start the login and vilification of the system installation, if the system is installed the user will be directed to the index page of the resource that is linked, which can be a theme or a grid, and if the system is not installed the system will direct the user to install.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$system_install = true;
+$system_version = "1.0.0";
+$system_option_select = "themes"; //grids, themes
+$system_name_select = "gridpage-basic";
+
+if($system_install == true){
+	include("app/".$system_option_select."/".$system_name_select."/index.php");
+}else{
+    header ("location: install.php");
+}
+
+?>
