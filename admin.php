64de5307d1ade3b88f065ebcffa1e54fc274775f
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-04 21:09:13 -0300

    v0.0.0.3 - Style file creation and validation administration panel
    This time we started creating the css stylesheet files. We also started working on access validation of the system administration page.

--- admin.php
+++ admin.php
@@ -1,1 +1,32 @@
-Page Admin
+<?php
+    $system_install = true;
+    $system_logged = true;
+    $system_version = "1.0.0";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+</head>
+<body>
+
+	<?php
+        
+        if($system_install == true){
+
+        	if($system_logged == true){
+               include("app/system/v".$system_version."/admin-painel.php");
+        	}else{
+        		//login
+        	}
+       
+        }else{
+
+        	//instalar
+
+        }
+
+	?>
+
+</body>
+</html>
