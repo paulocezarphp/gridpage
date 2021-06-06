fe12b36a5711e094b9f90d8359cff75a35402717
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-06 14:08:52 -0300

    v0.0.0.5 - Creating themes for the dashboard
    This time we started creating the themes for the user panel, we also started to create some icons that will be part of the system soon.

--- admin.php
+++ admin.php
@@ -2,20 +2,27 @@
     $system_install = true;
     $system_logged = true;
     $system_version = "1.0.0";
+
+    include("app/system/v".$system_version."/php/modules/style-color.php");
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Admin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="sortcut icon" href="<?php echo "app/system/v".$system_version."/images/icon-radius.png"; ?>" type="image/x-icon" />
 </head>
-<body>
+<body style="background: <?php echo $theme_color_body; ?>;">
 
 	<?php
         
         if($system_install == true){
 
         	if($system_logged == true){
+
                include("app/system/v".$system_version."/admin-painel.php");
+               
         	}else{
         		//login
         	}
---
+++ app/system/v1.0.0/php/modules/style-color.php
@@ -0,0 +1,99 @@
+<?php
+
+//Name theme painel user
+$theme_name = "basic";
+
+$theme_color_text = "#292929";
+$theme_color_border = "#ebedec";
+$theme_color_background = "#7599a9";
+$theme_color_background_text = "#fff";
+$theme_color_body = "#dce9ef";
+$theme_color_box = "#fff";
+$theme_color_shadow = "#ccc";
+$theme_color_text = "#fff";
+$theme_color_button_validate = "#008fc3";
+$theme_color_button_cancel = "#c42312";
+
+if($theme_name == "basic"){
+
+}elseif($theme_name == "fantasy"){
+
+	$theme_color_text = "";
+	$theme_color_border = "";
+	$theme_color_background = "";
+	$theme_color_background_text = "";
+	$theme_color_body = "";
+	$theme_color_box = "";
+	$theme_color_shadow = "";
+	$theme_color_text = "";
+	$theme_color_button_validate = "";
+	$theme_color_button_cancel = "";
+
+}elseif($theme_name == "illustrious"){
+
+	$theme_color_text = "";
+	$theme_color_border = "";
+	$theme_color_background = "";
+	$theme_color_background_text = "";
+	$theme_color_body = "";
+	$theme_color_box = "";
+	$theme_color_shadow = "";
+	$theme_color_text = "";
+	$theme_color_button_validate = "";
+	$theme_color_button_cancel = "";
+
+}elseif($theme_name == "modern"){
+
+	$theme_color_text = "";
+	$theme_color_border = "";
+	$theme_color_background = "";
+	$theme_color_background_text = "";
+	$theme_color_body = "";
+	$theme_color_box = "";
+	$theme_color_shadow = "";
+	$theme_color_text = "";
+	$theme_color_button_validate = "";
+	$theme_color_button_cancel = "";
+
+}elseif($theme_name == "levitation"){
+
+	$theme_color_text = "";
+	$theme_color_border = "";
+	$theme_color_background = "";
+	$theme_color_background_text = "";
+	$theme_color_body = "";
+	$theme_color_box = "";
+	$theme_color_shadow = "";
+	$theme_color_text = "";
+	$theme_color_button_validate = "";
+	$theme_color_button_cancel = "";
+
+}elseif($theme_name == "noturnal"){
+
+	$theme_color_text = "";
+	$theme_color_border = "";
+	$theme_color_background = "";
+	$theme_color_background_text = "";
+	$theme_color_body = "";
+	$theme_color_box = "";
+	$theme_color_shadow = "";
+	$theme_color_text = "";
+	$theme_color_button_validate = "";
+	$theme_color_button_cancel = "";
+
+}else{
+
+	$theme_color_text = "#292929";
+	$theme_color_border = "#ebedec";
+	$theme_color_background = "#7599a9";
+	$theme_color_background_text = "#fff";
+	$theme_color_body = "#dce9ef";
+	$theme_color_box = "#fff";
+	$theme_color_shadow = "#ccc";
+	$theme_color_text = "#fff";
+	$theme_color_button_validate = "#008fc3";
+	$theme_color_button_cancel = "#c42312";
+	
+}
+
+?>
