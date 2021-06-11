f389bfaecd8037c37187a9e3bb5f6b7578b765db
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-11 20:45:30 -0300

    v0.0.1.0 - Finishing Basic Class Structures
    This time, we started to create a file to store the system localization files so that it displays the panel language according to the user's localization. We also finalized the basic structures of the classes we had created in previous versions.

--- app/system/v1.0.0/php/class/class-translator.php
+++ app/system/v1.0.0/php/class/class-translator.php
@@ -1,8 +1,8 @@
 <?php
 
 class GPtranslate{
-    
-    public function get_text($language, $key_register, $text){
+	
+	public function get_text($language, $key_register, $text){
 
 	}
 
---
+++ app/system/v1.0.0/php/class/class-validate.php
@@ -0,0 +1,39 @@
+<?php
+
+class GPvalidate{
+	
+	public function email_user($text){
+
+	}
+
+	public function name_user($text){
+
+	}
+
+	public function nickname_user($text){
+
+	}
+
+	public function password_user($text){
+
+	}
+
+	public function name_database($text){
+
+	}
+
+	public function host_database($text){
+
+	}
+
+	public function root_database($text){
+
+	}
+
+	public function password_database($text){
+
+	}
+
+}
+
+?>
