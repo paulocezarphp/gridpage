4bbea19ad592ed3ace1f759eff6c7fa065a3de54
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-14 20:59:25 -0300

    v0.0.1.3 - Creating login panel
    This time we started to create the login panel, we created a css file to separate the login code from the rest of the app, soon we will adapt the login panel for mobile devices

--- app/system/v1.0.0/admin-login.php
+++ app/system/v1.0.0/admin-login.php
@@ -1,1 +1,23 @@
-Painel Login
+<div class="login">
+	<form method="POST" action="">
+		<div class="login-context">
+			<img src="<?php echo "app/system/v".$system_version."/images/icon.png"; ?>" class="login-image-icon">
+			<div class="login-title">GridPage</div>
+			<div class="login-description">Access your account</div>
+			<div class="login-label">Login</div>
+			<input type="text" name="login" class="login-input" 
+			autocomplete="off" placeholder="Digite seu login">
+			<div class="login-label">Password</div>
+			<input type="password" name="password" class="login-input" 
+			autocomplete="off" placeholder="Digite sua senhaa">
+
+			<div class="login-info-group">
+				<input type="checkbox" name="">
+				<div class="login-remember">To remember</div>
+			</div>
+
+			<button type="submit" class="loginbutton">Log In</button>
+			<input type="hidden" name="operacao" value="login">
+		</div>
+	</form>
+</div>
--- login.php
+++ login.php
@@ -24,6 +24,7 @@ include("app/system/v".$system_version."/php/modules/style-color.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="sortcut icon" href="<?php echo "app/system/v".$system_version."/images/icon-radius.png"; ?>" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="<?php echo "app/system/v".$system_version."/css/style-login.css"; ?>">
 </head>
 <body>
     <?php 
