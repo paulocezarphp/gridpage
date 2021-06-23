a5b026e26aec8999577ecb7255bade4c11959f00
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-22 21:06:05 -0300

    v0.0.2.0 - Creating database roles
    This time, we create a log file to return system errors to the user during use. We also created some functions that we will use to manipulate the database in the future.

--- app/system/v1.0.0/php/class/class-database.php
+++ app/system/v1.0.0/php/class/class-database.php
@@ -2,7 +2,41 @@
 
 class GPdatabase{
 	
-	
+	public function connect_db($db_name, $db_host, $db_user, $db_password, $db_prefix){
+
+	}
+
+	public function insert_register($sql){
+
+	} 
+
+	public function update_register($sql){
+		
+	}
+    
+    public function list_register($sql){
+		
+	}
+
+	public function delete_register($sql){
+		
+	}
+
+	public function create_table($sql){
+
+	}
+
+	public function delete_table($sql){
+		
+	}
+
+	public function update_table($sql){
+		
+	}
+
+	public function list_table($sql){
+		
+	}
 	
 }
 
---
+++ app/system/v1.0.0/php/class/class-log.php
@@ -0,0 +1,19 @@
+<?php
+
+class GPlog{
+	
+	public function add_register($type){
+        //add a record
+	}
+
+	public function list_register($number_page){
+        //list the records
+	}
+
+	public function clean_register(){
+        //delete log records
+	} 
+
+}
+
+?>
