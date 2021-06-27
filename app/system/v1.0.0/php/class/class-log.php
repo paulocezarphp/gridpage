7774ea7842ecdb55f781e6cf8c2be8cf89bfc328
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-27 14:36:24 -0300

    v0.0.2.5 -  Log file
    This time we start to implement the log class, in it we will save all system changes and manipulations, both errors and successful operations, with this we will be able to better understand while using the system what may be going wrong during the execution process. system.

--- app/system/v1.0.0/php/class/class-log.php
+++ app/system/v1.0.0/php/class/class-log.php
@@ -3,7 +3,94 @@
 class GPlog{
 	
 	public function add_register($type){
-        //add a record
+        
+        switch ($type) {
+        	case 'ok_system_install':
+        		# code...
+        		break;
+        	case 'ok_system_backup':
+        		# code...
+        		break;
+        	case 'ok_system_reboot':
+        		# code...
+        		break;
+        	case 'error_login_access':
+        		# code...
+        		break;
+        	case 'ok_login_access':
+        		# code...
+        		break;
+        	case 'ok_module_install':
+        		# code...
+        		break;
+        	case 'erro_module_install':
+        		# code...
+        		break;
+        	case 'ok_module_activate':
+        		# code...
+        		break;
+        	case 'erro_module_activate':
+        		# code...
+        		break;
+        	case 'ok_module_disable':
+        		# code...
+        		break;	
+        	case 'error_module_disable':
+        		# code...
+        		break;
+        	case 'ok_module_delete':
+        		# code...
+        		break;	
+        	case 'error_module_delete':
+        		# code...
+        		break;
+        	case 'ok_theme_activate':
+        		# code...
+        		break;
+        	case 'error_theme_activate':
+        		# code...
+        		break;
+        	case 'ok_theme_import':
+        		# code...
+        		break;
+        	case 'error_theme_import':
+        		# code...
+        		break;
+        	case 'ok_theme_delete':
+        		# code...
+        		break;
+        	case 'error_theme_delete':
+        		# code...
+        		break;
+        	case 'ok_grid_create':
+        		# code...
+        		break;
+        	case 'error_grid_create':
+        		# code...
+        		break;
+        	case 'ok_grid_edit':
+        		# code...
+        		break;
+        	case 'error_grid_edit':
+        		# code...
+        		break;
+        	case 'ok_grid_import':
+        		# code...
+        		break;
+        	case 'error_grid_import':
+        		# code...
+        		break;
+        	case 'ok_grid_delete':
+        		# code...
+        		break;
+        	case 'error_grid_delete':
+        		# code...
+        		break;
+        	default:
+        		# code...
+        		break;
+        }
+
 	}
 
 	public function list_register($number_page){
